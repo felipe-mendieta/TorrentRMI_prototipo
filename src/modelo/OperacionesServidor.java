c5cc076cd5f89f2c7f113f3427a79628ea545064
Estudiante <Estudiante@CCING-03-11>
2019-12-04 15:57:08 -0500

    cada cliente pide por partes por un hilo distinto funcionando. desconeccion de un host en construccion.

--- src/modelo/OperacionesServidor.java
+++ src/modelo/OperacionesServidor.java
@@ -5,7 +5,7 @@ import java.rmi.RemoteException;
 
 
 public interface OperacionesServidor extends Remote{
-    public boolean login(OperacionesCliente c) throws RemoteException;
+    public boolean login(OperacionesCliente c,String nombreDelArchivo) throws RemoteException;
     public long getSizeFile(String nombre) throws RemoteException;
     public int calcularMayor(int num1,int num2) throws RemoteException;
 }
