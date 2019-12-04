e3baafe6aaaddff7afe6e229e8af3c812f3daddc
linux <linux@ccing-02-01>
2019-12-04 10:47:27 -0500

    path agregado archivosRecibidos

--- src/modelo/OperacionesServidor.java
+++ src/modelo/OperacionesServidor.java
@@ -6,5 +6,6 @@ import java.rmi.RemoteException;
 
 public interface OperacionesServidor extends Remote{
     public boolean login(OperacionesCliente c) throws RemoteException;
+    public long getSizeFile(String nombre) throws RemoteException;
     public int calcularMayor(int num1,int num2) throws RemoteException;
 }
